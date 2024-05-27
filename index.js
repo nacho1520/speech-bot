cf067b666ddfb1c753675791da1d3e67698d0cc0
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-05-27 17:31:35 -0300

    managing voice speed

--- index.js
+++ index.js
@@ -4,18 +4,32 @@ window.addEventListener('load', () => {
     const submitBtn = document.querySelector('.submit-btn');
 
     const synth = window.speechSynthesis;
+    let voices;
+    let speed = 1;
 
+    const loadVoices = () => {
+        voices = synth.getVoices();
+        console.log(voices);
+    };
 
+    // in Google Chrome the voices are not ready on page load
+    if ("onvoiceschanged" in synth) {
+        synth.onvoiceschanged = loadVoices;
+    } else {
+        loadVoices();
+    }
 
     speedBtns.forEach((btn, index) => {
         btn.addEventListener('click', () => {
             speedBtns.forEach(btn => { btn.classList.remove('active') });
             btn.classList.add('active');
+            speed = btn.value;
         });
     });
 
     submitBtn.addEventListener('click', () => {
         const utterThis = new SpeechSynthesisUtterance(userInput.value);
+        utterThis.rate = speed;
         synth.speak(utterThis);
     });
 });
