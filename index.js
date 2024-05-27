4cffa166646473c279fc3df2e33b3039321902b3
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-05-27 17:11:57 -0300

    speech function

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 window.addEventListener('load', () => {
+    const userInput = document.querySelector('#user-text');
     const speedBtns = document.querySelectorAll('.speed-btn');
+    const submitBtn = document.querySelector('.submit-btn');
+
+    const synth = window.speechSynthesis;
+
+
 
     speedBtns.forEach((btn, index) => {
         btn.addEventListener('click', () => {
             speedBtns.forEach(btn => { btn.classList.remove('active') });
             btn.classList.add('active');
         });
+    });
+
+    submitBtn.addEventListener('click', () => {
+        const utterThis = new SpeechSynthesisUtterance(userInput.value);
+        synth.speak(utterThis);
     });
 });
