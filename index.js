10c52477d3be21983a716bcc562d98129f55d578
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-05-28 13:10:19 -0300

    voice dropdowns

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ window.addEventListener('load', () => {
     const userInput = document.querySelector('#user-text');
     const speedBtns = document.querySelectorAll('.speed-btn');
     const submitBtn = document.querySelector('.submit-btn');
+    const langBtn = document.querySelector('#lang-dropdown');
+    const langDisplay = document.querySelector('#lang-display');
+    const langList = document.querySelector('#lang-content');
+    const langOptions = document.querySelectorAll('#lang-option');
+    const voiceBtn = document.querySelector('#voice-dropdown');
+    const voiceDisplay = document.querySelector('#voice-display');
+    const voiceList = document.querySelector('#voice-content');
+    const voiceOptions = document.querySelectorAll('#voice-option');
 
     const synth = window.speechSynthesis;
     let voices;
@@ -31,5 +39,31 @@ window.addEventListener('load', () => {
         const utterThis = new SpeechSynthesisUtterance(userInput.value);
         utterThis.rate = speed;
         synth.speak(utterThis);
+    });
+
+    langBtn.addEventListener('click', () => {
+        langList.classList.toggle('active');
+    });
+    
+    voiceBtn.addEventListener('click', () => {
+        voiceList.classList.toggle('active');
+    });
+
+    langOptions.forEach((lang) => {
+        lang.addEventListener('click', () => {
+            langOptions.forEach(lang => { lang.classList.remove('active') });
+            lang.classList.add('active');
+            langDisplay.textContent = lang.textContent;
+            langList.classList.remove('active');
+        });
+    });
+
+    voiceOptions.forEach((voice) => {
+        voice.addEventListener('click', () => {
+            voiceOptions.forEach(voice => { voice.classList.remove('active') });
+            voice.classList.add('active');
+            voiceDisplay.textContent = voice.textContent;
+            voiceList.classList.remove('active');
+        });
     });
 });
